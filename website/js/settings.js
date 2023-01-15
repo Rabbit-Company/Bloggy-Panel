05efac9df5ad1af227e11b80faf8f119fe4b53b8
Ziga <ziga.zajc007@gmail.com>
2023-01-15 19:39:37 +0100

    Update

--- website/js/settings.js
+++ website/js/settings.js
@@ -6,4 +6,12 @@ loadData().then(() => {
 	document.getElementById("settings-lang").value = user.language;
 	document.getElementById("settings-theme").value = user.theme;
 	document.getElementById("settings-session").value = readData('sessionDuration');
+
+	if(user.fa_enabled === true){
+		document.getElementById("toggle-2fa-btn").innerText = "Disable";
+		document.getElementById("toggle-2fa-btn").className = "dangerButton font-bold inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md focus:outline-none sm:text-sm";
+	}else{
+		document.getElementById("toggle-2fa-btn").innerText = "Enable";
+		document.getElementById("toggle-2fa-btn").className = "successButton font-bold inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md focus:outline-none sm:text-sm";
+	}
 });
