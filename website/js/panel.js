de0ea75bf382e29f1034d8aae6900bdf3e681e81
Ziga <ziga.zajc007@gmail.com>
2023-01-15 08:38:38 +0100

    Update

--- website/js/panel.js
+++ website/js/panel.js
@@ -1,3 +1,31 @@
 loadData().then(() => {
 	startAuthenticator();
 });
+
+document.getElementById("search").addEventListener("keypress", (event) => {
+	if (event.key !== "Enter") return;
+	event.preventDefault();
+	window.location.assign("?search=" + document.getElementById("search").value);
+});
+
+document.getElementById("page").addEventListener("keypress", (event) => {
+	if (event.key !== "Enter") return;
+	event.preventDefault();
+	window.location.assign("?page=" + document.getElementById("page").value);
+});
+
+document.getElementById("dialog-button-cancel").addEventListener("click", () => {
+	hide('dialog');
+});
+
+document.getElementById("signout-link-mobile").addEventListener("click", () => {
+	logout();
+});
+
+document.getElementById("signout-link").addEventListener("click", () => {
+	logout();
+});
+
+document.getElementById("main-menu-toggle-btn").addEventListener("click", () => {
+	toggleMenu();
+});
