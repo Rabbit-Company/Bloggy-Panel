eb0c533ea80e6d14732be4b77e24f8262aff07a9
Ziga <ziga.zajc007@gmail.com>
2023-01-15 13:10:57 +0100

    Update

--- website/js/editor.js
+++ website/js/editor.js
@@ -1,5 +1,19 @@
 loadData().then(() => {
 	startAuthenticator();
+
+	let posts = JSON.parse(readData('posts'));
+
+	// Edit
+	if(parms.get("edit") != null && parms.get("edit").length >= 5) {
+		let id = parms.get("edit");
+		for(let i = 0; i < posts.length; i++){
+			if(posts[i].id !== id) continue;
+			document.getElementById("id").value = id;
+			document.getElementById("title").value = posts[i].title;
+			document.getElementById("description").value = posts[i].description;
+		}
+	}
+
 });
 
 document.getElementById("dialog-button-cancel").addEventListener("click", () => {
