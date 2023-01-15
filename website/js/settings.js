c259ae09bfe759281663858bf2f24f1811ef01a5
Ziga <ziga.zajc007@gmail.com>
2023-01-15 19:48:24 +0100

    Update

--- website/js/settings.js
+++ website/js/settings.js
@@ -14,4 +14,17 @@ loadData().then(() => {
 		document.getElementById("toggle-2fa-btn").innerText = "Enable";
 		document.getElementById("toggle-2fa-btn").className = "successButton font-bold inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md focus:outline-none sm:text-sm";
 	}
+
+	if(user.yubico === null || user.yubico == ''){
+		hide("remove-yubico-btn");
+	}else{
+		let yubico = user.yubico.split(";");
+		if (yubico.length >= 5) hide("add-yubico-btn");
+
+		let html = "";
+		for (let i = 0; i < yubico.length; i++) {
+			html += "<li class='passwordsBorderColor py-4 flex'><img class='h-10 w-10 rounded-full' src='/images/yubikey.png' alt='Yubico Key'><div class='ml-3'><p class='secondaryColor text-sm font-medium'>" + yubico[i] + "</p></div></li>";
+		}
+		document.getElementById('yubico-list').innerHTML = html;
+	}
 });
