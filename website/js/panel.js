bea7e02dc41ddac6b3029a0e21f8858c11dd9073
Ziga <ziga.zajc007@gmail.com>
2023-01-15 08:47:23 +0100

    Update

--- website/js/panel.js
+++ website/js/panel.js
@@ -1,5 +1,25 @@
 loadData().then(() => {
 	startAuthenticator();
+
+	let posts = JSON.parse(readData('posts'));
+
+	//Search
+	let search = null;
+	if(parms.get("search") != null && parms.get("search").length >= 1) {
+		fhide('pagination');
+		search = parms.get("search");
+		document.getElementById("search").value = search;
+		let tempArray = [];
+		search = search.toLowerCase();
+		for(let i = 0; i < posts.length; i++){
+			const title = posts[i].title;
+			const tag = posts[i].tag;
+			const keywords = posts[i].keywords;
+
+			if(title.includes(search) || tag.includes(search) || keywords.includes(search) || tempArray.push(posts[i]));
+		}
+		posts = tempArray;
+	}
 });
 
 document.getElementById("search").addEventListener("keypress", (event) => {
