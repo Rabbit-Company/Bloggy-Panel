2f818b382118466e5441c2878d73da9a336a5344
Ziga <ziga.zajc007@gmail.com>
2023-01-15 14:36:43 +0100

    Update

--- website/js/editor.js
+++ website/js/editor.js
@@ -67,6 +67,10 @@ document.getElementById("tabs-1-tab-1").addEventListener("click", () => {
 document.getElementById("tabs-1-tab-2").addEventListener("click", () => {
 	fhide("tabs-1-panel-1");
 	let content = document.getElementById("content").value;
-	document.getElementById('preview').innerHTML = marked.parse(content);
+	document.getElementById('preview').innerHTML = marked.parse(content, {
+		gfm: true,
+		breaks: true,
+		sanitizer: DOMPurify.sanitize
+	});
 	fshow("tabs-1-panel-2", "block");
 });
