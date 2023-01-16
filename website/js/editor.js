458159d5bc9d71aca5d5605cdf3a612e353bde1f
Ziga <ziga.zajc007@gmail.com>
2023-01-16 11:28:37 +0100

    Update

--- website/js/editor.js
+++ website/js/editor.js
@@ -24,6 +24,7 @@ loadData().then(() => {
 			document.getElementById("id").readOnly = true;
 			document.getElementById("btn-post").innerText = "Edit";
 
+			document.getElementById("preview_main_picture").src = posts[i].picture;
 			document.getElementById("preview_main_created").innerText = posts[i].created;
 		}
 	}else{
@@ -55,28 +56,6 @@ document.getElementById("btn-post").addEventListener("click", () => {
 	changeDialog(6);
 	show('dialog');
 });
-
-function compressImage(imageFile) {
-
-	console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
-	console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
-
-	var options = {
-		maxSizeMB: 1,
-		maxWidthOrHeight: 1920,
-		useWebWorker: true
-	}
-
-	imageCompression(imageFile, options).then(compressedFile => {
-		console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
-		console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
-
-		return compressedFile;
-	}).catch(error => {
-		console.log(error.message);
-		return null;
-	});
-}
 
 document.getElementById("title").addEventListener("input", () => {
 	if(document.getElementById("id").readOnly) return;
