1130b87fee8d73f72a627635ab762c4d97201d73
Ziga <ziga.zajc007@gmail.com>
2023-01-16 13:00:40 +0100

    Update

--- website/js/analytics.js
+++ website/js/analytics.js
@@ -2,3 +2,19 @@ loadData().then(() => {
 	startAuthenticator();
 
 });
+
+document.getElementById("signout-link").addEventListener("click", () => {
+	logout();
+});
+
+document.getElementById("signout-link-mobile").addEventListener("click", () => {
+	logout();
+});
+
+document.getElementById("main-menu-toggle-btn").addEventListener("click", () => {
+	toggleMenu();
+});
+
+document.getElementById("dialog-button-cancel").addEventListener("click", () => {
+	hide('dialog');
+});
